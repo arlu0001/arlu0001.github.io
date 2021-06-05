6a201d6b3399d0b13dc39d73ae82c66f740aec6f
arlu0001 <arlu0001@stud.hs-kl.de>
2021-06-05 13:05:26 +0200

    sogdof

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,16 +6,16 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "a343d01e8cc8e460d0526fb92f70b5f9",
+"assets/NOTICES": "35613d428a40b8c02d85e35fb0cdb431",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "cd9d5f66b8a46b1c501acc09488cedff",
-"/": "cd9d5f66b8a46b1c501acc09488cedff",
-"main.dart.js": "ccc26b9ddc679dbf553539adbce94351",
-"manifest.json": "105564294e5212994fc1209e94c51ef3",
-"version.json": "8d2c6310d0f7c1d4ff4a3fd3a94c0be2"
+"index.html": "680f4052f93c546109fba53b9f2c7309",
+"/": "680f4052f93c546109fba53b9f2c7309",
+"main.dart.js": "c658a96cb5c74d0a0be15e16e52e5e8e",
+"manifest.json": "0ceccd831badde5b973e7af798d27ec1",
+"version.json": "fb800d37cb51ce25f91a8114a375508b"
 };
 
 // The application shell files that are downloaded before a service worker can
