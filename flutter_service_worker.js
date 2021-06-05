c25b278925fab8f5ac05c5b633b4f2a64cff85ca
arlu0001 <arlu0001@stud.hs-kl.de>
2021-06-05 11:32:20 +0200

    hisdhfk

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4d7dadd6bab22fa03de61890c1d21ac3",
-"/": "4d7dadd6bab22fa03de61890c1d21ac3",
-"main.dart.js": "d52fd68939ec86341504b911f35e0cfd",
+"index.html": "4324bc09d3afd5e410f5602a005a1ca3",
+"/": "4324bc09d3afd5e410f5602a005a1ca3",
+"main.dart.js": "f05b735fe8768cdfcdcd6409f26c49f2",
 "manifest.json": "0ceccd831badde5b973e7af798d27ec1",
 "version.json": "fb800d37cb51ce25f91a8114a375508b"
 };
