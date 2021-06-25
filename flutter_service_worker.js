f24ef7cd35f8542616083c1ad1fb512d2b6218c7
arlu0001 <arlu0001@stud.hs-kl.de>
2021-06-25 13:37:23 +0200

    kqawfnoa

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -18,7 +18,7 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "index.html": "680f4052f93c546109fba53b9f2c7309",
 "/": "680f4052f93c546109fba53b9f2c7309",
-"main.dart.js": "87ed16396963439a4c2f8e3366d0915a",
+"main.dart.js": "f544bd8e7c39a0a07196e4d3e87ba3f1",
 "manifest.json": "0ceccd831badde5b973e7af798d27ec1",
 "version.json": "fb800d37cb51ce25f91a8114a375508b"
 };
